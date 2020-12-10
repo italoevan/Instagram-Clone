d97c7e5bd48ade593a741aa0b1916d563629da6c
Ítalo Évan <igbehh@gmail.com>
2020-12-10 20:26:19 -0300

    3º commit

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: "Instagram Clone",
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primaryColor: Colors.black),
     home: Home(),
